c9b98f6ba1341e0fee0bb2a8c696589b3a78e5c3
litman27 <litman.ramos.27@unsch.edu.pe>
2022-07-10 19:01:46 -0500

    cien

---
+++ contraseña.js
@@ -0,0 +1,10 @@
+function ir(){
+var c=123456789;
+var u="litman";
+  if(document.form.password.value==c && document.form.login.value==u){
+  alert ("bienvenidos a la clase de estadistica");
+window.location="inicio.html";}
+else {
+alert("porfavor ingrese, el nombre y usuarios correctos.");
+}
+}
